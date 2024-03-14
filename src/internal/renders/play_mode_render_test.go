5bab0a7dff08d726babd7ae5295f73137a59f26f
rafael-santiago <voidbrainvoid@tutanota.com>
2024-03-13 23:09:25 -0300

    Fix broken tests

--- src/internal/renders/play_mode_render_test.go
+++ src/internal/renders/play_mode_render_test.go
@@ -11,13 +11,13 @@ func TestPlayModeRender(t *testing.T) {
     eutherpeVars := &vars.EutherpeVars{}
     templatedInput := fmt.Sprintf("%s", vars.EutherpeTemplateNeedlePlayMode)
     output := PlayModeRender(templatedInput, eutherpeVars)
-    if output != "Play" {
+    if output != "&#x25BA" {
         t.Errorf("PlayModeRender() seems not to be rendering accordingly : %s\n", output)
     } else {
         eutherpeVars.Player.NowPlaying = mplayer.SongInfo { FilePath: "/dev/42/carnavoyeur.mp3" }
         output = PlayModeRender(templatedInput, eutherpeVars)
-        if output != "Stop" {
-            t.Errorf("PlayModeRender() seems not to be rendering accordingly.\n")
+        if output != "&#x25A0" {
+            t.Errorf("PlayModeRender() seems not to be rendering accordingly : %s\n", output)
         }
     }
 }
