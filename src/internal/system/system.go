c4c20c289a3167a108e582d82b160b8d982f2c7d
rafael-santiago <voidbrainvoid@tutanota.com>
2024-05-26 15:23:59 -0300

    Run sudo when shuting down the system

--- src/internal/system/system.go
+++ src/internal/system/system.go
@@ -16,8 +16,11 @@ func Reboot() error {
 
 func doShutdown(params ...string) error {
     var customPath string
+    var sudo string
     if flag.Lookup("test.v") != nil {
         customPath = "../system"
+    } else {
+        sudo = "sudo "
     }
-    return exec.Command(path.Join(customPath, "shutdown"), params...).Run()
+    return exec.Command(sudo + path.Join(customPath, "shutdown"), params...).Run()
 }
