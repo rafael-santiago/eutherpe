43dc96d855c5353a0fe47357e94681d0be289866
rafael-santiago <voidbrainvoid@tutanota.com>
2024-05-29 22:22:41 -0300

    Create certifcate root path when it does not previously exist

--- src/internal/actions/generate_certificate.go
+++ src/internal/actions/generate_certificate.go
@@ -4,27 +4,36 @@ import (
     "internal/vars"
     "net/url"
     "os/exec"
+    "os"
     "fmt"
     "path"
 )
 
 func GenerateCertificate(eutherpeVars *vars.EutherpeVars, _ *url.Values) error {
+    certRootPath := path.Join(eutherpeVars.HTTPd.PubRoot, "cert")
+    _, err := os.Stat(certRootPath)
+    if err != nil {
+        err = os.MkdirAll(certRootPath, 0777)
+        if err != nil {
+            return fmt.Errorf("Unable to create '%s' directory : '%s'", err.Error())
+        }
+    }
     cmd := exec.Command("openssl",
-                           "req",
-                           "-new",
-                           "-newkey",
-                           "rsa:2048",
-                           "-days",
-                           "3650",
-                           "-nodes",
-                           "-x509",
-                           "-keyout",
-                           path.Join(eutherpeVars.ConfHome, "eutherpe.priv"),
-                           "-out",
-                           path.Join(eutherpeVars.HTTPd.PubRoot, "cert", "eutherpe.cer"),
-                           "-subj",
-                           "/CN=" + eutherpeVars.HTTPd.Addr)
-    err := cmd.Run()
+                        "req",
+                        "-new",
+                        "-newkey",
+                        "rsa:2048",
+                        "-days",
+                        "3650",
+                        "-nodes",
+                        "-x509",
+                        "-keyout",
+                        path.Join(eutherpeVars.ConfHome, "eutherpe.priv"),
+                        "-out",
+                        path.Join(eutherpeVars.HTTPd.PubRoot, "cert", "eutherpe.cer"),
+                        "-subj",
+                        "/CN=" + eutherpeVars.HTTPd.Addr)
+    err = cmd.Run()
     if err != nil {
         return fmt.Errorf("Error when trying to generate a new certificate : '%s'", err.Error())
     }
