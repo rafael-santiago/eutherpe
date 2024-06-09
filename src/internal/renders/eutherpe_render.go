de5ffdce31fdc123b22dc47fc7ac4c82531416f8
rafael-santiago <voidbrainvoid@tutanota.com>
2024-06-09 15:33:27 -0300

    src/internal/renders: Fix Eutherpe render when detecting arm

--- src/internal/renders/eutherpe_render.go
+++ src/internal/renders/eutherpe_render.go
@@ -10,7 +10,7 @@ func EutherpeRender(templatedInput string, eutherpeVars *vars.EutherpeVars) stri
     if len(eutherpeVars.APPName) == 0 && !strings.HasPrefix(runtime.GOARCH, "arm") {
         eutherpeVars.APPName = "Eutherpe"
     } else if len(eutherpeVars.APPName) == 0 {
-        eutherpeVars.APPName = "Euther&Pi"
+        eutherpeVars.APPName = "Euther-&Pi;"
     }
     return strings.Replace(templatedInput,
                            vars.EutherpeTemplateNeedleEutherpe,
